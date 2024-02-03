9cd5934cf1c5622c595e5fbc10206972ab5eb51d
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-03 20:59:23 +0500

    first commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Head  from "next/head";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <div>AboutPage</div>
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import  Head  from "next/head";
+
+const IndexPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Index Page</title>
+    </Head>
+    <div>IndexPage</div>
+    </>
+  );
+};
+
+export default IndexPage;
