7471cea33185c5e10228613fb4b91b297eae919b
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-04 15:04:58 +0500

    added footer comonent

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,20 @@
+import Telegram from "@mui/icons-material/Telegram"
+import Instagram from "@mui/icons-material/Instagram"
+import Youtube from "@mui/icons-material/YouTube"
+import { Box, ButtonGroup, Typography } from "@mui/material"
+import { format } from 'date-fns'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box padding={'20px'} sx={{ display: 'flex', justifyContent: "space-between", alignItems: 'center', bgcolor: 'primary.main', color: 'white' }}>
+      <Typography>© {format(new Date(), 'yyyy')} INote. All Right Reserved.</Typography>
+
+      <Box sx={{ display: 'flex', gap: '15px', cursor: 'pointer' }}>
+        <Telegram />
+        <Instagram />
+        <Youtube />
+      </Box>
+    </Box>
   )
 }
 
