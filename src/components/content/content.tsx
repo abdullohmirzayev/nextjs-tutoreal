c7dc915c9bd74208b84dce22552d2109ad177869
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-05 09:50:30 +0500

    added #6. SIdebar component

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 const Content = () => {
-  return <Box sx={{ width: "70%" }}>Content</Box>;
+  return <Box sx={{ width: "70%", height: '200vh' }}>Content</Box>;
 };
 
 export default Content;
